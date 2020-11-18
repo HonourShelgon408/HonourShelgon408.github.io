66ac1f447b231aa75b356fcb78334a22579c11d9
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-18 19:55:04 +0000

    Update db.js
    update snapshot

--- js/db.js
+++ js/db.js
@@ -2,5 +2,5 @@
 db.collection('noteId').onSnapshot(function(snapshot){
     //takes snapshot of database at that time and returns it in the callback function
     //returns this snapshot to us
-    console.log(snapshot.docChanges())
+    console.log("SNAPSHOT: " , snapshot.docChanges())
 });
