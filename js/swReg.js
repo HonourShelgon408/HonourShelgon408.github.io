3fc973672cc8b665f247f17fc5c14e2b8f137243
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-18 17:38:26 +0000

    More changes to Firebase
    Trying to get it to work

--- js/swReg.js
+++ js/swReg.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     'use strict';
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js')
-        .then(function(reg){console.log("Service worker registered ", reg)
-            .catch(function(error){console.log("Error creating service worker: ", error)
+        navigator.serviceWorker.register('./sw.js').then(function(reg){
+            console.log("Service worker registered!").catch(function(error){
+                console.log("Error creating service worker: ", error)
             });
         });
     }
