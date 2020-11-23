fe41c965905a5dd2e8f1f1e46752c3a45fded36a
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-23 23:04:15 +0000

    Update sw.js
    version number changes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var cacheName = 'scheduler-v5';
-var dynamicCache = 'dynamic-v5';
+var cacheName = 'scheduler-v6';
+var dynamicCache = 'dynamic-v6';
 var filesToCache = [ /* array of filenames referenced by relativity */
   '/', /* just the index page default - request urls */
   '/index.html',
