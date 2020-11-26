a27cf25af1d4e09ee82499bc11f0b315a81efd74
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-26 23:29:25 +0000

    trying a reload method

--- js/db.js
+++ js/db.js
@@ -26,6 +26,7 @@ db.collection('noteId').onSnapshot(function(snapshot){
         else if(dbChange === 'removed'){
             console.log(dbChange, " " , change.doc.id);
             deleteNote(change.doc.id);
+            reloadContent();
         }
     //console.log(change, " ", change.doc.data()), " ", change.doc.id;
     });
