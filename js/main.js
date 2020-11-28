eebfdce6f13d30cf9850edd6f62cf9c9289f34b8
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-28 16:39:18 +0000

    Update main.js
    console.log for the css selector

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ window.mobileCheck = function() { //returns boolean "true" if mobile device
      /** unrender note from the DOM  */
      function deleteNote (id) {
         if(id != null && id != undefined){
+          console.log(`.note[data-id='${id}']`);
           const note = document.querySelector(`.note[data-id='${id}']`);        /**css attribute selector=> get element of class with attribute that is 'data-id=*insert id of element*' */
           console.log("deleteNote note element: " + id);                        /**backticks allow the insertion of variables */
           console.log(note);
