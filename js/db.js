555533f137fb8599b8751489db82a894ae9b44e5
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2020-11-30 23:29:29 +0000

    Not much
    Testing rawDatabase page for viewing data
    Added Icon to error page
    Changed Contact Us icon from Mail to Arrow Icon

--- js/db.js
+++ js/db.js
@@ -29,6 +29,8 @@ db.collection('noteId').onSnapshot(function(snapshot){
         }
     //console.log(change, " ", change.doc.data()), " ", change.doc.id;
     });
+
+    $("#raw_database").append().snapshot.val().key.value;
 });
 
 
