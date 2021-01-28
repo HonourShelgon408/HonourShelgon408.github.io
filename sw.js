1c6d4dc1cbc293d6dfdf3a51a632d6e68c9d571c
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2021-01-28 11:47:54 +0000

    Update version to 3.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var versionNum = 'v3.2';
+var versionNum = 'v3.3';
 var cacheName = 'scheduler-' + versionNum;
 var dynamicCache = 'dynamic-'+ versionNum;
 var filesToCache = [ /* array of filenames referenced by relativity */
