7865dda1cc463a8a2840ad334ae754d33217696d
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2021-02-09 13:29:05 +0000

    Create firebase-messaging-sw.js
    made this file as per requirements

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-messaging.js');
+importScripts('firebase-config-details.js');
+
+console.log(firebase_global);
