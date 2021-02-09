0352d1fe4d384a588d2f3a7df0208fae64711ac9
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2021-02-09 16:08:31 +0000

    SW things
    Added another log to see how to access notification data
    Added pushmanager code to service worker registration

--- js/swReg.js
+++ js/swReg.js
@@ -3,6 +3,12 @@ window.onload = () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./sw.js').then(function(reg){
             console.log("Service worker registered!"/* , reg */);
+            return reg.pushManager.getSubscription().then(function(subscription){
+                reg.pushManager.subscribe()({
+                    userVisibleOnly: true,
+                    applicationServerKey: determineAppServerKey()
+                })
+            })
         });
     }
 }
