86cca3f0a198f6971e3bafdd8b5c53f4e1c33bd6
ClientAd-HonourShelgon408 <HonourShelgon408@gmail.com>
2021-02-10 16:01:51 +0000

    s
    Seemed to add a '?' typo at the bottom of the index scripts
    Indentations appeared to have gone from main.js, added back in

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import moment from 'moment'
 
 window.mobileCheck = function() { //returns "true" if mobile device
     let check = false;
@@ -29,71 +28,71 @@ const overview = document.querySelector('#overview');
 
 /** insert line breaks into string */
 function convertBreaks(str){
-for(var i = 0; i<str.length; i++){
-    if(str[i].match(/\n/g)||[]){
-        str[i] = String.fromCharCode(13);
+    for(var i = 0; i<str.length; i++){
+        if(str[i].match(/\n/g)||[]){
+            str[i] = String.fromCharCode(13);
+        }
     }
-}
 }
 
 /**render note to the DOM */
 function addNote(data, id) {
-let html = ``;
-const fontSize = 12;
-const letters = (content.scrollWidth / fontSize) - 5;
-const title = data.title.substring(0,letters);
-const body = data.body.substring(0,letters);
+    let html = ``;
+    const fontSize = 12;
+    const letters = (content.scrollWidth / fontSize) - 5;
+    const title = data.title.substring(0,letters);
+    const body = data.body.substring(0,letters);
 
-//TODO -- CHANGE NOTIFICATION ICON DEPENDENT ON RELATED RECORDS FOR REMINDERS
-//notifications -> notifications_active -> notifications_none
+    //TODO -- CHANGE NOTIFICATION ICON DEPENDENT ON RELATED RECORDS FOR REMINDERS
+    //notifications -> notifications_active -> notifications_none
 
-html = `
-<div class="card-panel row note hoverable" data-id='${id}'>
-    <div class="note-move">
-        <i class="material-icons move-icon">toc</i>
-    </div>
-    <div class="note-details">
-`;
+    html = `
+    <div class="card-panel row note hoverable" data-id='${id}'>
+        <div class="note-move">
+            <i class="material-icons move-icon">toc</i>
+        </div>
+        <div class="note-details">
+    `;
 
-if(title.length >= 1){
-    html += `<div class="note-title sidenav-trigger trunc" data-id='${id}' data-target="update-form">${title}</div>`;
-}
-if(body.length >= 1){
-    html += `<div class="note-body sidenav-trigger trunc" data-id='${id}' data-target="update-form">${body}</div>`;
-}
-html += `</div> <!-- closing note-details class div -->`;
-const alarms = false;
-if(alarms == false){
+    if(title.length >= 1){
+        html += `<div class="note-title sidenav-trigger trunc" data-id='${id}' data-target="update-form">${title}</div>`;
+    }
+    if(body.length >= 1){
+        html += `<div class="note-body sidenav-trigger trunc" data-id='${id}' data-target="update-form">${body}</div>`;
+    }
+    html += `</div> <!-- closing note-details class div -->`;
+    const alarms = false;
+    if(alarms == false){
+        html += 
+        `<div class="note-options">
+            <i class="material-icons data-id="${id}">notifications_none</i><input type="text" class="reminderDatePicker datepicker">
+        </div>`;
+
+        // html += 
+        // `
+        // <div class="input-field inline">
+        //     <input id="email_inline" type="email" class="validate">
+        //     <label for="email_inline">Email</label>
+        //     <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+        // </div>
+        // `;
+
+    }
+
+    // if(alarms == true){
+    //     html += `
+    //     <div class="note-options">
+    //         <i class="material-icons bell-icon data-id="${id}">notifications_active</i>
+    //     </div> `;
+    // }
     html += 
-    `<div class="note-options">
-        <i class="material-icons data-id="${id}">notifications_none</i><input type="text" class="reminderDatePicker datepicker">
+        `<div class="note-delete">
+            <i class="material-icons delete-icon" data-id="${id}">delete_outline</i>
+        </div>
     </div>`;
 
-    // html += 
-    // `
-    // <div class="input-field inline">
-    //     <input id="email_inline" type="email" class="validate">
-    //     <label for="email_inline">Email</label>
-    //     <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-    // </div>
-    // `;
-
-}
-
-// if(alarms == true){
-//     html += `
-//     <div class="note-options">
-//         <i class="material-icons bell-icon data-id="${id}">notifications_active</i>
-//     </div> `;
-// }
-html += 
-    `<div class="note-delete">
-        <i class="material-icons delete-icon" data-id="${id}">delete_outline</i>
-    </div>
-</div>`;
-
-content.innerHTML += html;
-};
+    content.innerHTML += html;
+    };
 
 
 /** derender note from the DOM  */
