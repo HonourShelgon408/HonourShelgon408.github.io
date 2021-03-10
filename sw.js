addbd7a5d95e8c28327916351461f68470e7f1a8
abAWDG6 <honourshelgon408@gmail.com>
2021-03-10 14:08:44 +0000

    update version num

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var versionNum = 'v3.25';
+var versionNum = 'v3.26';
 var cacheName = 'scheduler-' + versionNum;
 var dynamicCache = 'dynamic-'+ versionNum;
 var filesToCache = [ /* array of filenames referenced by relativity */
